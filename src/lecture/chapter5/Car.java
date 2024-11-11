3644a245c73bc782453b072d82bc2ecebecf32c6
Matthias Berg-Neels <matthiasuweberg@icloud.com>
2024-11-11 18:38:18 +0100

    CarExample - printCarDetails()

--- src/lecture/chapter5/Car.java
+++ src/lecture/chapter5/Car.java
@@ -10,19 +10,38 @@ class Car {
   String brand;
   Color wheelColor;
 
+  // Konstruktor-Definition
+  Car(String color, int hp, String licensePlate, String brand, Color wheelColor){
+    this.color = color;
+    this.hp = hp;
+    this.licensePlate = licensePlate;
+    this.brand = brand;
+    this.wheelColor = wheelColor;
+
+    this.speed = 0.0;
+  }
+
   // Definition der Instanz-Methoden für die Klasse Auto
   void accelerate(double deltaSpeed){
     if(deltaSpeed > 0) {
-      speed += deltaSpeed;
+      this.speed += deltaSpeed;
     }
   }
 
   void brake(){
     // um 5 km/h abbremsen
-    speed -= 5;
-    if(speed < 0){
-      speed = 0;
+    this.speed -= 5;
+    if(this.speed < 0){
+      this.speed = 0;
     }
+  }
+
+  void printCarDetails(){
+    System.out.println("Mein Auto ist ein " + myCar.brand
+      + " in der Farbe " + myCar.color
+      + " mit " + myCar.hp + " PS"
+      + " und hat das Nummernschild " + myCar.licensePlate
+      + " Räderfarbe: " + myCar.wheelColor.name + "(HexCode: " + myCar.wheelColor.colorHexCode + ")");
   }
 
 
--- src/lecture/chapter5/CarRun.java
+++ src/lecture/chapter5/CarRun.java
@@ -11,30 +11,27 @@ public class CarRun {
     myWheelColor.colorHexCode = 0xFF9797;
     myWheelColor.name = "Pink";
 
-    myCar = new Car();
+    myCar = new Car("Pink", 500, "HH-BB 1234", "Mitsubishi", myWheelColor);
+    /*
     myCar.color = "Pink";
     myCar.hp = 500;
     myCar.brand = "Mitsubishi";
     myCar.licensePlate = "HH-BB 1234";
     myCar.wheelColor = myWheelColor;
+     */
 
 
-    Car yourCar = new Car();
+    Car yourCar = new Car("Schwarz", 100, "MA-MA 5678", "Mercedes", new Color()
+    );
+    /*
     yourCar.color = "Schwarz";
     yourCar.hp = 100;
     yourCar.brand = "Mercedes";
     yourCar.licensePlate = "MA-MA 5678";
+     */
 
-    System.out.println("Mein Auto ist ein " + myCar.brand
-                        + " in der Farbe " + myCar.color
-                        + " mit " + myCar.hp + " PS"
-                        + " und hat das Nummernschild " + myCar.licensePlate
-                        + " Räderfarbe: " + myCar.wheelColor.name + "(HexCode: " + myCar.wheelColor.colorHexCode + ")");
-
-    System.out.println("Mein Auto ist ein " + yourCar.brand
-      + " in der Farbe " + yourCar.color
-      + " mit " + yourCar.hp + " PS"
-      + " und hat das Nummernschild " + yourCar.licensePlate);
+    myCar.printCarDetails();
+    yourCar.printCarDetails();
 
     System.out.println("Mein Auto fährt " + myCar.speed + " km/h");
     System.out.println("Dein Auto fährt " + yourCar.speed + " km/h");
